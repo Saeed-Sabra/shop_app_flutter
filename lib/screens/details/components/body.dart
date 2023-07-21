727707051c9766eab55528413a508c632d66c155
Saeed Sabra <saeedsabra565@gmail.com>
2023-07-21 22:18:22 +0300

    finish adding color and size and description

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
 import 'package:shop_app/models/Product.dart';
+import 'package:shop_app/screens/details/components/color_and_size.dart';
+import 'package:shop_app/screens/details/components/description.dart';
 import 'package:shop_app/screens/details/components/product_title_with_image.dart';
 
 class Body extends StatelessWidget {
@@ -19,13 +21,24 @@ class Body extends StatelessWidget {
               children: [
                 Container(
                   margin: EdgeInsets.only(top: size.height * 0.35),
-                  height: 500,
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPadding,
+                    right: kDefaultPadding,
+                  ),
+                  // height: 500,
                   decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(24),
                       topRight: Radius.circular(24),
                     ),
+                  ),
+                  child: Column(
+                    children: [
+                      ColorAndSize(product: product),
+                      Description(product: product),
+                    ],
                   ),
                 ),
                 ProductTitleWithImage(product: product),
